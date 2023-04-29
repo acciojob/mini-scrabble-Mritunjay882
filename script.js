b14c9e8e04c0b42b85874b39462721f2a8a7078f
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 21:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+// script.js
+var evaluatedText = document.getElementById("evaluatedText");
+var letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", function () {
+  var text = evaluatedText.value;
+  var count = text.length;
+  letterCount.innerText = count;
+});
